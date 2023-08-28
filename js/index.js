89b568b1211daf700879520ea0ed29ae702f528d
Yuri Tagomori <goncalvessyuri@gmail.com>
2023-08-28 11:49:19 -0300

    add navbar style interaction

--- js/index.js
+++ js/index.js
@@ -10,3 +10,46 @@ router.handle()
 
 window.onpopstate = () => router.handle()
 window.route = () => router.route()
+
+// Navbar
+
+const btnLearnMore = document.querySelector('#btn-explore')
+const linkHome = document.querySelector('.nav-home');
+const linkUniverse = document.querySelector('.nav-universe');
+const linkExploration = document.querySelector('.nav-exploration');
+
+linkHome.addEventListener('click', function(){
+  navHome()
+})
+
+linkUniverse.addEventListener('click', function(){
+  navUniverse()
+})
+
+linkExploration.addEventListener('click', function(){
+  navExploration()
+})
+
+if (btnLearnMore) {
+  btnLearnMore.addEventListener('click', function() {
+    navExploration()
+  })
+}
+
+function navHome() {
+  linkUniverse.classList.remove('active');
+  linkExploration.classList.remove('active');
+  linkHome.classList.add('active');
+}
+
+function navUniverse() {
+  linkHome.classList.remove('active');
+  linkExploration.classList.remove('active');
+  linkUniverse.classList.add('active');
+}
+
+function navExploration() {
+  linkHome.classList.remove('active');
+  linkUniverse.classList.remove('active');
+  linkExploration.classList.add('active');
+}
